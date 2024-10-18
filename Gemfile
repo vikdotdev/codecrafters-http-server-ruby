80da9fe2609c8ccb9974075404fe2b194ed029c1
Viktor Habchak <vikdotdev@gmail.com>
2024-10-18 18:40:17 +0200

    basic server

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ source "https://rubygems.org"
 
 # Add gems here if you need to use them.
 # Make sure to run `bundle install` before running tests or submitting your code.
+
+gem "pry", "~> 0.14.2"
--- app/server.rb
+++ app/server.rb
@@ -1,10 +1,72 @@
 require "socket"
+require "pry"
 
-# You can use print statements as follows for debugging, they'll be visible when running tests.
-print("Starting server...")
+class Logger
+  def info(value)
+    puts "#{prefix}#{value}"
+  end
+
+  private
+
+  def prefix
+    "[SERVER]: "
+  end
+end
+
+Status = Data.define(:code) do
+  def to_s = "#{code} #{human_code}#{CRLF}"
+  def human_code
+    case code
+    when 200 then "OK"
+    end
+  end
+end
+
+Header = Data.define(:name, :value) do
+  def to_s = "#{name.to_s.split("_").map(&:capitalize).join("-")}: #{value}#{CRLF}"
+end
+
+class Headers < Array
+  def to_s = map(&:to_s).join
+end
+
+Response = Data.define(:status, :headers, :body) do
+  def to_s = "#{status_line}#{headers}#{CRLF}#{body}"
+  def status_line = "#{HTTP_VERSION} #{status}"
+end
+
+Route = Data.define(:constraint) do
+  def match?(path) = path.match(constraint)
+end
+
+CRLF = "\r\n"
+HTTP_VERSION = "HTTP/1.1"
 
 server = TCPServer.new("localhost", 4221)
-client_socket, client_address = server.accept
+logger = Logger.new
 
-print client_socket
-print client_address
+logger.info("Listening...")
+
+loop do
+  Thread.start(server.accept) do |client|
+    puts "Received request"
+
+    Status.new(200) => status
+    Headers.new => headers
+    headers << Header.new(name: :content_type, value: "text/plain")
+    headers << Header.new(name: :connection, value: "close")
+
+    Response.new(status:, headers:, body: "Hello!") => response
+
+    logger.info(response.to_s)
+
+    client.puts response.to_s
+    client.close
+
+    puts "Request completed"
+  end
+rescue Interrupt
+  puts
+  puts 'Exiting'
+  exit(2)
+end
